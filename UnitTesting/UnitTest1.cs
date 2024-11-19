a9f7013228961ad3f32b4ad3e8fb9d978f7058e9
DanteaExe <jorgedantex@gmail.com>
2024-11-19 13:17:35 -0600

    Initial commit

---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,42 @@
+using CalculatorLib;
+namespace UnitTesting;
+
+public class UnitTest1
+{
+    // Arrange
+    // The data to run the function
+    // Act
+    // Call the function to test
+    // Assert
+    // Compare the excpected vs the actual result
+    // Edge cases
+    // TDD -> Test Driven Development
+    // dotnet new xunit
+    [Fact]
+    public void Add2and2()
+    {
+        // Arrange
+        double a = 2;
+        double b = 2;
+        double expected = 4;
+        Calculator calc = new();
+        // Act
+        double actual = calc.Add(a, b);
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Add3And2()
+    {
+        // Arrange
+        double a = 3;
+        double b = 2;
+        double expected = 5;
+        Calculator calc = new();
+        // Act
+        double actual = calc.Add(a, b);
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
